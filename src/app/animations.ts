84e2126c11686c0c318b86426276eb139432cd88
Guilherme Nicchio Pinotte <guilherme.pinotte@serpro.gov.br>
2024-06-10 17:37:31 -0300

    New const trigger animation created

--- src/app/animations.ts
+++ src/app/animations.ts
@@ -1,4 +1,10 @@
-import { animate, state, style, transition, trigger } from "@angular/animations";
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger,
+} from '@angular/animations';
 
 export const highlightedStateTrigger = trigger('highlightedState', [
   state(
@@ -25,4 +31,43 @@ export const highlightedStateTrigger = trigger('highlightedState', [
     ),
     animate(200),
   ]),
-])
+]);
+
+export const shownStateTrigger = trigger('shownState', [
+  transition(':enter', [
+    style({
+      opacity: 0,
+    }),
+    animate(
+      300,
+      style({
+        opacity: 1,
+      })
+    ),
+  ]),
+  transition(':leave', [
+    animate(
+      300,
+      style({
+        opacity: 0,
+      })
+    ),
+  ]),
+]);
+
+export const checkButtonTrigger = trigger('checkButton', [
+  transition('* => checked', [
+    animate(
+      '400ms ease-in',
+      style({
+        transform: 'scale(0.4)',
+      })
+    ),
+    animate(
+      '400ms ease-out',
+      style({
+        transform: 'scale(1)',
+      })
+    ),
+  ]),
+]);
--- src/app/lista-tarefas/lista-tarefas.component.ts
+++ src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,13 +4,17 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { highlightedStateTrigger } from '../animations';
+import {
+  checkButtonTrigger,
+  highlightedStateTrigger,
+  shownStateTrigger,
+} from '../animations';
 
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger],
+  animations: [highlightedStateTrigger, shownStateTrigger, checkButtonTrigger],
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
